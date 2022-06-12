4e9626e5979108d41194f01387ba59f586e8bb22
tebecheri10 <tebecheri10@gmail.com>
2022-06-11 23:32:29 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import styled from '@emotion/styled'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+     
+    </div>
+  )
+}
+
+export default App
